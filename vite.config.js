c0007c9bd7674f7ebd8921b191b77839613ae99b
IraMira333 <irinat333@gmail.com>
2023-02-16 17:18:32 +0200

    Додає код для переходів на сторінки

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import FullReload from 'vite-plugin-full-reload';
 export default defineConfig({
   root: 'src',
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        nested: resolve(__dirname, 'src/apartment.html'),
+        nested: resolve(__dirname, 'src/hotel.html'),
+        nested: resolve(__dirname, 'src/restaurant.html'),
+      },
+    },
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
