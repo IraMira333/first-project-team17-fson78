5f533d33fd94feec0e793b98b685b5f3091071b7
IraMira333 <irinat333@gmail.com>
2023-02-22 19:43:54 +0200

    one

--- src/js/modprice1.js
+++ src/js/modprice1.js
@@ -1,16 +1,16 @@
 (() => {
   const refs = {
-    openModprice1Btn: document.querySelector('[data-modprice1-open]'),
-    closeModprice1Btn: document.querySelector('[data-modprice1-close]'),
-    modprice1: document.querySelector('[data-modprice1]'),
+    openModpriceoneBtn: document.querySelector('[data-modpriceone-open]'),
+    closeModpriceoneBtn: document.querySelector('[data-modpriceone-close]'),
+    modpriceone: document.querySelector('[data-modpriceone]'),
     body: document.querySelector('body'),
   };
 
-  refs.openModprice1Btn.addEventListener('click', toggleModprice1);
-  refs.closeModprice1Btn.addEventListener('click', toggleModprice1);
+  refs.openModpriceoneBtn.addEventListener('click', toggleModpriceone);
+  refs.closeModpriceoneBtn.addEventListener('click', toggleModpriceone);
 
-  function toggleModprice1() {
-    refs.modprice1.classList.toggle('is-hidden');
+  function toggleModpriceone() {
+    refs.modpriceone.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
   }
 })();
